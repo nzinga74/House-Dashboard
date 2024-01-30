f5e46d6f3bc7226d5f563e6528a157c2c17066c4
unknown <Yuu-rita>
2024-01-30 22:48:21 +0100

    New Version

--- src/componentes/form/Input.js
+++ src/componentes/form/Input.js
@@ -1,21 +1,20 @@
-import styles from './Input.module.css'
+import styles from "./Input.module.css";
+import { Formik, Form, Field } from "formik";
 
-function Input({type, text, name, placeholder, handleOnChange, value}){
-  return(
+function Input({ type, text, name, placeholder, handleOnChange, value }) {
+  return (
     <div className={styles.form_control}>
-        <label htmlFor = {name}>{text}:</label>
-        <input 
-        type = {type}
-        name = {name}
-        id ={name}
-        placeholder ={placeholder}
-        onChange = {handleOnChange}
-        value = {value}
-        />
-
-        
+      <label htmlFor={name}>{text}:</label>
+      <Field
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        onChange={handleOnChange}
+        value={value}
+      />
     </div>
   );
 }
 
-export default Input
+export default Input;
--- src/componentes/funcionario/FuncionarioForm.js
+++ src/componentes/funcionario/FuncionarioForm.js
@@ -1,42 +1,72 @@
-import styles from './FuncionarioForm.module.css'
-import Input from '../form/Input'
-import SubmitButton from '../form/SubmitButton';
+import styles from "./FuncionarioForm.module.css";
+import Input from "../form/Input";
+import SubmitButton from "../form/SubmitButton";
+import { Formik, Form, Field } from "formik";
+import styleform from "../form/Input.module.css";
+import * as Yup  from "yup"
 
-
-function FuncionarioForm({btnText}){
-    return (
-        <form className={styles.form}>
-            <Input
-             type = "text" 
-             text="Nome" 
-             name = "name"
-              placeholder="Insira o seu nome"
+function FuncionarioForm({ btnText }) {
+    const FuncionarioSchema = Yup.object().shape({
+        name: Yup.string().min(4,"Nome muito curto").required("Requirido"),
+         surname: Yup.string().min(4,"Nome muito curto").required("Requirido"),
+         number: Yup.number().min(9, "Deve ter 9 digitos").required("Requirido"),
+         email: Yup.string().email("Deve ser email").required("Requirido")
+    })
+    
+  return (
+    <div className={styles.formcontainer}>
+      <Formik
+       validationSchema={FuncionarioSchema}
+        onSubmit={(values) => console.log(values)}
+        initialValues={{
+          name: "",
+          surname: "",
+          email: "",
+          number: 0,
+          email: "",
+        }}
+      >
+        {({ values,errors, touched }) => (
+          <Form className={styles.form}>
+            <div className={styleform.form_control}>
+              <label>Nome: </label>
+              <Field
+                type="text"
+                text="Nome"
+                name="name"
+                placeholder="Insira o seu nome"
               />
-
-            <Input
-             type = "text" 
-             text="Sobrenome" 
-             name = "sobrenome"
+              {errors.name && touched.name ? (<div> {errors.name}</div>):null}
+            </div>
+            <Field
+              type="text"
+              text="Sobrenome"
+              name="surname"
               placeholder="Insira o seu sobrenome"
-              />
-             <Input
-             type = "number" 
-             text="Telefone" 
-             name = "telefone"
+              
+            />
+             {errors.surname && touched.surname ? (<div> {errors.surname}</div>):null}
+            <Field
+              type="number"
+              text="Telefone"
+              name="number"
               placeholder="Insira o seu telefone"
-              />
-             <Input
-             type = "text" 
-             text="Email" 
-             name = "name"
+            />
+             {errors.number && touched.number ? (<div> {errors.number}</div>):null}
+            <Field
+              type="text"
+              text="Email"
+              name="email"
               placeholder="Insira o seu email"
-              />
+            />
+             {errors.email && touched.email ? (<div> {errors.email}</div>):null}
 
-             <SubmitButton text={btnText}/>
-
-        </form>
-    );
-
+            <SubmitButton type="submit" text={btnText} />
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
-export default FuncionarioForm
+export default FuncionarioForm;
