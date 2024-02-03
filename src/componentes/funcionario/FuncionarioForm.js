02b03f8d3f7367c4f5c8a522ab7e9eee67935428
unknown <Yuu-rita>
2024-02-03 12:21:26 +0100

    formularios

--- src/componentes/funcionario/FuncionarioForm.js
+++ src/componentes/funcionario/FuncionarioForm.js
@@ -5,6 +5,7 @@ import { Formik, Form, Field } from "formik";
 import styleform from "../form/Input.module.css";
 import * as Yup  from "yup"
 
+
 function FuncionarioForm({ btnText }) {
     const FuncionarioSchema = Yup.object().shape({
         name: Yup.string().min(4,"Nome muito curto").required("Requirido"),
@@ -36,31 +37,40 @@ function FuncionarioForm({ btnText }) {
                 name="name"
                 placeholder="Insira o seu nome"
               />
-              {errors.name && touched.name ? (<div> {errors.name}</div>):null}
+              {errors.name && touched.name ? (<div className={styleform.error}> {errors.name}</div>):null}
             </div>
+            <div className={styleform.form_control}>
+              <label>Sobrenome:</label>
             <Field
               type="text"
               text="Sobrenome"
-              name="surname"
+              name ="surname"
               placeholder="Insira o seu sobrenome"
               
             />
-             {errors.surname && touched.surname ? (<div> {errors.surname}</div>):null}
+            {errors.surname && touched.surname ? (<div className={styleform.error}> {errors.surname}</div>):null}
+            </div>
+             
+             <div className={styleform.form_control}> 
+             <label>Telefone:</label>
             <Field
               type="number"
               text="Telefone"
               name="number"
               placeholder="Insira o seu telefone"
             />
-             {errors.number && touched.number ? (<div> {errors.number}</div>):null}
+             {errors.number && touched.number ? (<div className={styleform.error}> {errors.number}</div>):null}
+             </div>
+             <div className={styleform.form_control}>
+              <label>Email:</label>
             <Field
               type="text"
               text="Email"
               name="email"
               placeholder="Insira o seu email"
             />
-             {errors.email && touched.email ? (<div> {errors.email}</div>):null}
-
+             {errors.email && touched.email ? (<div className={styleform.error}> {errors.email}</div>):null}
+             </div>
             <SubmitButton type="submit" text={btnText} />
           </Form>
         )}
--- src/componentes/manutencao/ManutencaoForm.js
+++ src/componentes/manutencao/ManutencaoForm.js
@@ -1,53 +1,97 @@
 import styles from './ManutencaoForm.module.css'
 import Input from '../form/Input'
 import SubmitButton  from '../form/SubmitButton'
+import { Formik, Form, Field } from "formik";
+import styleform from "../form/Input.module.css";
+import * as Yup  from "yup"
 
 
 function Manutencao({btnText}){
+    const ManutencaoSchema = Yup.object().shape({
+        descricao: Yup.string().min(10,"Descrição muito curta").required("Requirido"),
+         data_inicio: Yup.date().required("Requirido"),
+         data_conclusao: Yup.date().required("Requirido"),
+          custo: Yup.number().min(9, "Deve ter 9 digitos").required("Requirido"),
+         funcionario: Yup.string().min(4, "Nome muito curto").required("Requirido")
+    })
 
     return(
-        <form className={styles.form}>
-            <Input
-             type = "text" 
-             text="Descrição" 
-             name = "descrição"
-              placeholder="Insira a descrição da manutenção"
+        <div className={styles.formcontainer}>
+             <Formik
+       validationSchema={ManutencaoSchema}
+        onSubmit={(values) => console.log(values)}
+        initialValues={{
+          descricao: "",
+          data_inicio: 0,
+          data_conclusao: 0,
+          custo: 0,
+          funcionario: "",
+        }}
+      >
+         {({ values,errors, touched }) => (
+          <Form className={styles.form}>
+            <div className={styleform.form_control}>
+              <label>Descrição: </label>
+              <Field
+                type="text"
+                text="Descrição"
+                name="descricao"
+                placeholder="Insira a descrição da manutenção"
               />
-            <Input
-             type = "date" 
-             text="Data de inicio" 
-             name = "data_inicio"
-              placeholder="Insira a data de inicio da manutenção"
+              {errors.descricao && touched.descricao ? (<div className={styleform.error}> {errors.descricao}</div>):null}
+            </div>
+
+            <div className={styleform.form_control}>
+              <label>Data de inicio: </label>
+              <Field
+                type="date"
+                text="Data de inicio"
+                name="data_inicio"
+                placeholder="Insira a data de inicio da manutenção"
               />
-              <Input
-             type = "date" 
-             text="Data de conclusão" 
-             name = "data_conclusão"
-              placeholder="Insira a data de conclusão da manutenção"
+              {errors.data_inicio && touched.data_inicio ? (<div className={styleform.error}> {errors.data_inicio}</div>):null}
+            </div>
+
+            <div className={styleform.form_control}>
+              <label>Data de conclusão: </label>
+              <Field
+                type="date"
+                text="Data de conclusão"
+                name="data_conclusão"
+                placeholder="Insira a data de conclusão da manutenção"
               />
-              <Input
-             type = "number" 
-             text="Custo" 
-             name = "custo"
-              placeholder="Insira o custo da manutenção"
+              {errors.data_conclusao && touched.data_conclusao ? (<div className={styleform.error}> {errors.data_conclusao}</div>):null}
+            </div>
+
+            <div className={styleform.form_control}>
+              <label>Custo: </label>
+              <Field
+                type="number"
+                text="Custo"
+                name="custo"
+                placeholder="Insira o custo da manutenção"
               />
-              <Input
-             type = "name" 
-             text="Nome" 
-             name = "nome"
-              placeholder="Insira o nome do funcionario"
+              {errors.custo && touched.custo ? (<div className={styleform.error}> {errors.custo}</div>):null}
+            </div>
+
+            <div className={styleform.form_control}>
+              <label>Funcionário: </label>
+              <Field
+                type="text"
+                text="Funcionario"
+                name="funcionario"
+                placeholder="Insira o nome do funcionario"
               />
+              {errors.funcionario && touched.funcionario ? (<div className={styleform.error}> {errors.funcionario}</div>):null}
+            </div>
 
               <SubmitButton text={btnText}/>
-
-              
- 
-             
-
-
-
-        </form>
-    )
+             </Form>
+              )}
+              </Formik>
+            
+        </div>
+    );
 
 }
 
